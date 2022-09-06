47d7a53d3067342f2822c55cd574ff24e0d373b4
mstreutker <mark.streutker@gmail.com>
2022-09-06 22:35:14 +0200

    put logic in function, removed empty lines

--- parse_schedule.py
+++ parse_schedule.py
@@ -4,6 +4,72 @@ from bs4 import BeautifulSoup
 import requests
 import pandas as pd
 
+def parse_tables (tables):
+
+    rows = []
+    headers=[]
+    reset_header=True
+    rowspan = 1
+    for table in tables:
+        for child in table.children:
+        
+            column_index = 0
+            
+            for td in child:
+                row = []
+                try:
+                    style = td.get('style')
+                    colspan = int(td.get('colspan'))
+                    content = td.text.replace('\n', '').strip()
+                    if content == '':
+                        continue
+                    #content = content.text.replace('\n','')
+                    if style == 'background-color: lightgray; vertical-align: top':
+                        if reset_header:
+                            headers = []
+                            reset_header=False
+                        #print ('HEADER')
+                        i = 0
+                    # print(colspan)
+                        
+                        while i < colspan:
+                            headers.append(content)
+                            i+=1
+                    elif style == 'background-color: #dce6f2; vertical-align: top':
+                        #print('SESSION')
+                
+                    
+                        if colspan == 1:
+                            #print (content)
+                            #print (column_index)
+                            #print(rowspan)
+                            reset_header = True
+                            if column_index == 0:
+                                rowspan -= 1
+                                column_index += rowspan
+                                rowspan = int(td.get('rowspan'))  
+                            #print (column_index)
+                            header = headers[column_index]
+
+                            #row.append(content)
+                            row={"session": content, "category": header}
+
+
+                        #row.append(header[column_index])
+                            column_index+=1
+                except:
+                    continue
+                
+                #print (row)
+                if len(row) > 0:
+                    rows.append(row)
+    #print (header)
+    #print (rows[0])
+
+    df = pd.DataFrame(rows)
+    return df
+
+
 url = "https://2022.pycon.de/schedule-full-table/"
 #url = 'https://2022.pycon.de/scheduleT/'
 #url='https://2022.pycon.de/program/'
@@ -20,65 +86,10 @@ trs = soup.select('tr[style*=";background-color:"]')
 
 #print (soup.find_all('table'))
 
-rows = []
-headers=[]
-reset_header=True
-rowspan = 1
+tables = soup.find_all('table')
 
-for child in soup.find_all('table')[0].children:
- 
-    column_index = 0
-    
-    for td in child:
-        row = []
-        try:
-            style = td.get('style')
-            colspan = int(td.get('colspan'))
-            content = td.text.replace('\n', '').strip()
-            #content = content.text.replace('\n','')
-            if style == 'background-color: lightgray; vertical-align: top':
-                if reset_header:
-                    headers = []
-                    reset_header=False
-                #print ('HEADER')
-                i = 0
-            # print(colspan)
-                
-                while i < colspan:
-                    headers.append(content)
-                    i+=1
-            elif style == 'background-color: #dce6f2; vertical-align: top':
-                #print('SESSION')
-        
-            
-                if colspan == 1:
-                    print (content)
-                    print (column_index)
-                    print(rowspan)
-                    reset_header = True
-                    if column_index == 0:
-                        rowspan -= 1
-                        column_index += rowspan
-                        rowspan = int(td.get('rowspan'))  
-                    print (column_index)
-                    header = headers[column_index]
+df = parse_tables(tables)
 
-                    #row.append(content)
-                    row={"session": content, "category": header}
-
-
-                #row.append(header[column_index])
-                    column_index+=1
-        except:
-            continue
-        
-        #print (row)
-        if len(row) > 0:
-            rows.append(row)
-#print (header)
-print (rows[0])
-
-df = pd.DataFrame(rows)
 
 print(df)
 
